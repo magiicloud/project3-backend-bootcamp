6be21fb64854d4b033f21942f13cdb914e1dcde2
magiicloud <hongyun_rc@outlook.com>
2024-03-26 14:27:01 +0800

    amended seed data for carts to add created and updated

--- db/seeders/20240326050435-secondary-seeds.ts
+++ db/seeders/20240326050435-secondary-seeds.ts
@@ -33,14 +33,20 @@ module.exports = {
       {
         active: true,
         user_id: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
       },
       {
         active: false,
         user_id: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
       },
       {
         active: false,
         user_id: 3,
+        createdAt: new Date(),
+        updatedAt: new Date(),
       },
     ]);
 
