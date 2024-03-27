9c61c4b786f0805b7636f431c14712b329399a8a
magiicloud <hongyun_rc@outlook.com>
2024-03-27 15:04:04 +0800

    amended itemsController to show data from RoomItem and Room

--- controllers/itemsController.ts
+++ controllers/itemsController.ts
@@ -1,10 +1,17 @@
 import { Request, Response } from "express";
-import { Item } from "../db/models";
+import { Item, RoomItem, Room } from "../db/models";
 
 export class ItemsController {
   async getAllItems(req: Request, res: Response) {
     try {
-      const output = await Item.findAll();
+      const output = await Item.findAll({
+        include: [
+          {
+            model: RoomItem,
+            include: [{ model: Room, attributes: ["name"] }],
+          },
+        ],
+      });
       return res.json(output);
     } catch (err) {
       return res.status(400).json({ error: true, msg: (err as Error).message });
