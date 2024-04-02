cf2cd38f71833613f7cdace9d719bee341565f90
patrickkok <pkok1996@gmail.com>
2024-04-02 22:38:47 +0800

    Implement transactions

--- controllers/buildingController.ts
+++ controllers/buildingController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { Building, BuildingUser, User, Room } from "../db/models";
+import { Building, BuildingUser, User, Room, sequelize } from "../db/models";
 import { Sequelize } from "sequelize-typescript";
 import { Dialect, Transaction } from "sequelize";
 
@@ -11,13 +11,6 @@ interface RoomAttributes {
   height: Float64Array;
   building_id: number;
 }
-
-// const sequelize = new Sequelize({
-//   database: process.env.DB_NAME,
-//   username: process.env.DB_USERNAME,
-//   dialect: process.env.DB_DIALECT as Dialect,
-//   models: [Building, BuildingUser, Room, User], // Import your model classes
-// });
 
 export class BuildingsController {
   async getAllBuildings(req: Request, res: Response) {
@@ -38,16 +31,20 @@ export class BuildingsController {
   async AddNewBuilding(req: Request, res: Response) {
     const building = req.body.building;
     const rooms = req.body.rooms;
-    // const transaction: Transaction = await sequelize.transaction();
+    const transaction: Transaction = await sequelize.transaction();
     try {
-      const newBuilding = await Building.create(building);
+      const newBuilding = await Building.create(building, {
+        transaction,
+      });
       rooms.map(async (obj: RoomAttributes) => {
         const newRoom = { ...obj };
         newRoom["building_id"] = newBuilding.id;
-        await Room.create(newRoom);
+        await Room.create(newRoom, { transaction });
       });
+      await transaction.commit();
       return res.json(newBuilding);
     } catch (err) {
+      await transaction.rollback();
       return res.status(400).json({ error: true, msg: err });
     }
   }
